a2517d503cf90f8d45fb80bd6674bcd720d807ec
lee-shun <lee970802@163.com>
2021-09-20 12:23:10 -0400

    update

--- include/test/test_simple_command.hpp
+++ include/test/test_simple_command.hpp
@@ -14,11 +14,35 @@
  *
  ******************************************************************************/
 
-#ifndef _TEST_SIMPLE_COMMAND_HPP
-#define _TEST_SIMPLE_COMMAND_HPP
+#ifndef __TEST_SIMPLE_COMMAND_HPP__
+#define __TEST_SIMPLE_COMMAND_HPP__
+
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/QuaternionStamped.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <ros/ros.h>
+#include <ros/time.h>
 
 #include <iostream>
-#include <ros/ros.h>
-class test_simple_command {};
+class TestSimpleCommand {
+
+private:
+  ros::NodeHandle nh;
+
+  ros::Time begin_time;
+
+  ros::Subscriber vehicle_att_subscriber;
+
+  geometry_msgs::QuaternionStamped vehical_att;
+
+  void vehical_att_cb(const geometry_msgs::QuaternionStamped::ConstPtr &msg);
+
+public:
+  TestSimpleCommand();
+  ~TestSimpleCommand();
+
+  int run();
+};
 
 #endif /* TEST_SIMPLE_COMMAND_HPP */
--- src/test/test_simple_command.cpp
+++ src/test/test_simple_command.cpp
@@ -1,24 +1,49 @@
 /*******************************************************************************
-*
-*   Copyright (C) 2021 Lee Ltd. All rights reserved.
-*
-*   @Filename: test_simple_command.cpp
-*
-*   @Author: lee-shun
-*
-*   @Email: 2015097272@qq.com
-*
-*   @Date: 2021-09-19
-*
-*   @Description: 
-*
-******************************************************************************/
+ *
+ *   Copyright (C) 2021 Lee Ltd. All rights reserved.
+ *
+ *   @Filename: test_simple_command.cpp
+ *
+ *   @Author: lee-shun
+ *
+ *   @Email: 2015097272@qq.com
+ *
+ *   @Date: 2021-09-19
+ *
+ *   @Description:
+ *
+ ******************************************************************************/
 
+#include "ros/init.h"
 #include <test/test_simple_command.hpp>
-int main(int argc, char** argv){
-    ros::init(argc, argv, "test_simple_command_node");
-    ros::NodeHandle nh;
-    ROS_INFO("hello world!");
-return 0;
+
+TestSimpleCommand::TestSimpleCommand() {
+  vehicle_att_subscriber = nh.subscribe<geometry_msgs::QuaternionStamped>(
+      "dji_osdk_ros/attitude", 10, &TestSimpleCommand::vehical_att_cb, this);
+}
+TestSimpleCommand::~TestSimpleCommand() {}
+
+void TestSimpleCommand::vehical_att_cb(
+    const geometry_msgs::QuaternionStamped::ConstPtr &msg) {
+  vehical_att = *msg;
 }
 
+int TestSimpleCommand::run() {
+
+  ros::Rate rate(1);
+  begin_time = ros::Time::now();
+
+  while (ros::ok()) {
+    ROS_INFO("Hello");
+    ros::spinOnce();
+    rate.sleep();
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "test_simple_command_node");
+  TestSimpleCommand node;
+  node.run();
+  return 0;
+}
