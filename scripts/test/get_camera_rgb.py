37fbc565ccc716dd9c8766b8b8a56670ff7d1e2d
lee-shun <lee970802@163.com>
2021-09-24 16:41:06 -0400

    update the image

---
+++ scripts/test/get_camera_rgb.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
+
+# ------------------------------------------------------------------------------
+#
+#   Copyright (C) 2021 Lee Ltd. All rights reserved.
+#
+#   @Filename: get_camera_rgb.py
+#
+#   @Author: lee-shun
+#
+#   @Date: 2021-09-24
+#
+#   @Email: 2015097272@qq.com
+#
+#   @Description:
+#
+# ------------------------------------------------------------------------------
+
+import rospy
+from dji_osdk_ros.srv import _SetupCameraStream
+from sensor_msgs.msg import Image
+
+
+class GetImageNode(object):
+    def __init__(self):
+        self.image_frame = Image()
+        self.rate = rospy.Rate(10)
+
+        rospy.wait_for_service("setup_camera_stream")
+        self.set_camera_cli = rospy.ServiceProxy("setup_camera_stream",
+                                                 _SetupCameraStream)
+
+        rospy.Subscriber("dji_osdk_ros/main_camera_images",
+                         Image, self.image_cb)
+
+        self.image_pub = rospy.Publisher(
+            '/camera/rgb/image_raw', Image, queue_size=10)
+
+    def image_cb(self, msg):
+        self.image_frame = msg
+
+    def run(self):
+
+        set_camera_handle = _SetupCameraStream.SetupCameraStreamRequest()
+        set_camera_handle.cameraType = set_camera_handle.MAIN_CAM
+        set_camera_handle.start = 0
+        result = self.set_camera_cli(set_camera_handle)
+
+        print("the result of setting camera is", result)
+
+        while not rospy.is_shutdown():
+            self.image_pub.publish(self.image_frame)
+            self.rate.sleep()
+
+
+if __name__ == '__main__':
+    rospy.init_node('get_image_node', anonymous=True)
+
+    node = GetImageNode()
+    node.run()
