cad4b4647c24f3f38ca28a88dcc68519f591eb25
qiaolinhan <qiaolinhan@mac.local>
2021-09-27 09:34:32 -0400

    tryunet000

--- scripts/try001.py
+++ scripts/try001.py
@@ -1,31 +1,42 @@
-import torch
-import torchvision
+# try YOLO detect images
 import numpy as np
-
-import os
+import argparse
+import time
 import cv2
-import matplotlib.pyplot as plt
+import os
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help = "path to input images")
+ap.add_argument("-i", "--YOLO", required = True, help = "base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	            help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	            help="threshold when applying non-maxima suppression")
+args = vars(ap.parse_args())
 
-# testing te camera
-video_session = cv2.VideoCapture(0)
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
 
-# conver 2 RGB
-def grab_frame(cap):
-    _, frame = cap.read()
-    return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (15, 8))
-ax2.set_title("Mask")
-ax1.set_xticks([])
-ax1.set_yticks([])
-ax2.set_xticks([])
-ax2.set_yticks([])
-
-# Create two image objects to picture on top of the axes defined above
-im1 = ax1.imshow(grab_frame(video_session))
-im2 = ax2.imshow(grab_frame(video_session))
-#%%
-plt.ion()
-plt.show()
-
+# load our input image and grab its spatial dimensions
+image = cv2.imread(args["image"])
+(H, W) = image.shape[:2]
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+# construct a blob from the input image and then perform a forward
+# pass of the YOLO object detector, giving us our bounding boxes and
+# associated probabilities
+blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+	swapRB=True, crop=False)
+net.setInput(blob)
+start = time.time()
+layerOutputs = net.forward(ln)
+end = time.time()
+# show timing information on YOLO
+print("[INFO] YOLO took {:.6f} seconds".format(end - start))
