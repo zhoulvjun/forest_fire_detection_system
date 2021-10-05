1ec88bb622e2a6c3a37d014f07dd04b750abc329
qiaolinhan <742954173@qq.com>
2021-10-05 17:28:41 -0400

    tri spliting dataset, not yet

--- scripts/simplifying/unetutils000.py
+++ scripts/simplifying/unetutils000.py
@@ -20,68 +20,26 @@
 import torch
 import torchvision
 from unetlightdata000 import loadDataset
-from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader, dataset
 from torch.utils.data import Subset
+from torchvision.transforms import Compose, ToTensor, Resize
+from sklearn import tree
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def save_checkpoint(state, filename = "unetlight_checkpoint.pth"):
     print(" ===> saving checkpoint")
     torch.save(state, filename)
 
-# def load_checkpoint(checkpoint, model):
-#     print("===> loading checkpoint")
-#     model.load_state_dict(checkpoint["state_dict"])
-
-def train_val_dataset(dataset, val_split=0.25):
-    train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=val_split)
-    datasets = {}
-    datasets['train'] = Subset(dataset, train_idx)
-    datasets['val'] = Subset(dataset, val_idx)
-    return datasets
-
-
-def get_loaders(train_dir, train_maskdir, val_dir, val_maskdir,
-                batch_size, train_transform, val_transform,
-                num_workers = 4, pin_memory = True):
-
-    # train data set 
-    train_ds = loadDataset( 
-        image_dir=train_dir,
-        mask_dir=train_maskdir,
-        transform=train_transform
-    )
-
-    train_loader = DataLoader(
-        train_ds,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        pin_memory=pin_memory,
-        shuffle=True
-    )
-
-    # val data set
-    val_ds = CarvanaDataset( 
-        image_dir=val_dir,
-        mask_dir=val_maskdir,
-        transform=val_transform
-    )
-
-    val_loader = DataLoader(
-        val_ds,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        pin_memory=pin_memory,
-        shuffle=False
-    )
-    return train_loader, val_loader
-
-def check_accuracy(loader, model, device = "cuda"):
+def check_performance(loader, model, device = DEVICE):
     num_corrrect = 0
     num_pixels = 0
     dice_score = 0
     model.eval()
 
-    with torch.no_grad():
+    # for segmentation, dice score might be more important than the accuracy
+    with torch.no_grad(): 
         for x, y in loader:
             x = x.to(device)
             y = y.to(device)
@@ -90,15 +48,23 @@ def check_accuracy(loader, model, device = "cuda"):
             num_corrrect += (preds == y).sum()
             num_pixels += torch.numel(preds)
             dice_score += (2 * (preds * y).sum()) / (( preds + y ).sum() + 1e-8)
-
-    print(
-        f" Got {num_corrrect}/{num_pixels} with acc {num_corrrect/num_pixels * 100: .2f}"
-        )
-
+    # accuracy
+    print(f" Got {num_corrrect}/{num_pixels} with acc {num_corrrect/num_pixels * 100: .2f}")
+    # dice score
     print(f"Dice Score: {dice_score/len(loader)}")
 
     model.train()
 
+def train_val_split(self, dataset):
+    for i in path(dataset):
+        trainset, valset = train_test_split(dataset, )
+
+
+DataLoader(dataset, batch_size=1, shuffle=False, sampler=None,
+           batch_sampler=None, num_workers=0, collate_fn=None,
+           pin_memory=False, drop_last=False, timeout=0,
+           worker_init_fn=None, *, prefetch_factor=2,
+           persistent_workers=False)       
 def save_predictions_as_imgs(loader, model, folder = "saved_images/",
                              device = "cuda"):
     model.eval()
