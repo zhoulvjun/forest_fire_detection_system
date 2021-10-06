1fbb6d0504299fc309524e8d8774eaf4a7511fde
qiaolinhan <742954173@qq.com>
2021-10-05 22:39:35 -0400

    trying to split dataset

--- scripts/simplifying/unetutils000.py
+++ scripts/simplifying/unetutils000.py
@@ -24,14 +24,14 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader, dataset
 from torch.utils.data import Subset
 from torchvision.transforms import Compose, ToTensor, Resize
-from sklearn import tree
+from sklearn.model_selection import train_test_split
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def save_checkpoint(state, filename = "unetlight_checkpoint.pth"):
     print(" ===> saving checkpoint")
     torch.save(state, filename)
-
+    
 def check_performance(loader, model, device = DEVICE):
     num_corrrect = 0
     num_pixels = 0
@@ -55,31 +55,12 @@ def check_performance(loader, model, device = DEVICE):
 
     model.train()
 
-def train_val_split(self, dataset):
-    for i in path(dataset):
-        trainset, valset = train_test_split(dataset, )
+trainset, valset = train_test_split(loadDataset, random_state = 0.2, shuffle = False) # original, shuffle = True
 
-
-DataLoader(dataset, batch_size=1, shuffle=False, sampler=None,
-           batch_sampler=None, num_workers=0, collate_fn=None,
-           pin_memory=False, drop_last=False, timeout=0,
-           worker_init_fn=None, *, prefetch_factor=2,
-           persistent_workers=False)       
-def save_predictions_as_imgs(loader, model, folder = "saved_images/",
-                             device = "cuda"):
-    model.eval()
-    for idx, (x, y) in enumerate(loader):
-        x = x.to(device = device)
-        with torch.no_grad():
-            preds = torch.sigmoid(model(x))
-            preds = (preds > 0.5).float()
-
-        torchvision.utils.save_image(
-            preds, f"{folder}/pred_{idx}.png"
-        )
-        torchvision.utils.save_image(
-            y.unsqueeze(1), f"{folder}/pred_{idx}.png"
-        )
-
-    model.train()
+trainset = DataLoader(trainset, batch_size=1, shuffle = False, sampler = None)
+# DataLoader(dataset, batch_size=1, shuffle=False, sampler=None,
+#            batch_sampler=None, num_workers=0, collate_fn=None,
+#            pin_memory=False, drop_last=False, timeout=0,
+#            worker_init_fn=None, *, prefetch_factor=2,
+#            persistent_workers=False)       
 
