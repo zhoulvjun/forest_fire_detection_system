1b614934ce526cb4f4a76728ec9e793e08dbc0bd
qiaolinhan <742954173@qq.com>
2021-10-08 17:19:55 -0400

    modified the up sampling parts

--- scripts/unetsimplified/unetlight000.py
+++ scripts/unetsimplified/unetlight000.py
@@ -20,7 +20,7 @@ class Conv0(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Conv0, self).__init__()
         self.conv0 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 7, 1, 1, bias = False), # kernel = 7, stride = 1, padding = 1
+            nn.Conv2d(in_channels, out_channels, 7, 1, 3, bias = False), # kernel = 7, stride = 1, padding = 3
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace = True),
         )
@@ -36,18 +36,18 @@ class DWconv(nn.Module):
         super(DWconv, self).__init__()
         self.dwconv = nn.Sequential(
         nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, groups=1),
-        nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, groups=1),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0, groups=1),
         )
 
     def forward(self, x):
         return self.dwconv(x)
 
-class squeeze(nn.Module):
+class Fire(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(squeeze, self).__init__()
+        super(Fire, self).__init__()
         self.downSQ = nn.Sequential(
             # fire?
-            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=1, bias=False),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
             nn.ReLU(inplace = True),
             # pip install torch-dwconv did not work well, could try again
             # dwconv
@@ -56,23 +56,33 @@ class squeeze(nn.Module):
             nn.ReLU(inplace = True),
         )
     def forward(self, x):
-        out = self.downSQ(x)
+        return self.downSQ(x)
 
-class Conv1(nn.Module):
+class Conv33(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(Conv1, self).__init__()
-        self.conv1 = nn.Sequential(
+        super(Conv33, self).__init__()
+        self.conv3 = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias = False), # kernel = 3, stride = 1, padding = 1
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace = True),
         )
-
     def forward(self, x):
-        return self.conv1(x)
+        return self.conv3(x)
 
-class desqueeze(nn.Module):
+class Conv11(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(desqueeze, self).__init__()
+        super(Conv11, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1, 1, 0, bias = False), # kernel = 1, stride = 1, padding = 0
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace = True),
+        )
+    def forward(self, x):
+        return self.conv11(x)
+
+class deFire(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(deFire, self).__init__()
         self.upSQ = nn.Sequential(
             # defire?
             nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = 1, padding = 1, bias = False),
@@ -86,12 +96,21 @@ class desqueeze(nn.Module):
     def forward(self, x):
         return self.upSQ(x)
 
+class upblock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(upblock, self).__init__()
+        self.up = nn.Sequential(
+            deFire(in_channels, out_channels*2),
+            Conv33(out_channels*2, out_channels)
+        )
+    def forward(self, x):
+        return self.up(x)
+
 class finalconv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(finalconv, self).__init__()
         self.final = nn.Sequential(
-            Conv1(in_channels, out_channels),
-            nn.Conv2d(out_channels, out_channels, kernel_size=1)
+            Conv11(in_channels, out_channels),
         )
 
     def forward(self, x):
@@ -107,64 +126,89 @@ class unetlight(nn.Module):
                  features = [55, 27, 13]):
         super(unetlight, self).__init__()
         self.ups = nn.ModuleList()
-        self.downs = nn.ModuleList()
-        self.pools = nn.MaxPool2d(kernel_size=3, stride=3)
-
-        # begin layer, conv0
-        self.begin = Conv0(in_channels, begin_channels)
+        self.pools = nn.MaxPool2d(kernel_size=3, stride=1)
 
         # down sampling part
-        # self.downs.append(Conv0(in_channels, begin_channels))
-        for feature in features:
-            self.downs.append(squeeze(begin_channels, feature))
-            begin_channels = feature
+        # add pools, skip
+        self.down00 = Conv0(in_channels, begin_channels)
+        self.down01 = nn.ModuleList()
+        # self.down02 = nn.ModuleList()
+        # self.down03 = nn.ModuleList()
+        self.down04 = nn.ModuleList()
+        self.down05 = nn.ModuleList()
+
+        # add skip
+        self.down01.append(Fire(begin_channels, features[0]))
+        self.down01.append(Fire(features[0], features[0]))
+        # add pools
+        self.down02 = Fire(features[0], features[0])
+        # add skip
+        self.down03 = Fire(features[0], features[1])
+        # add pools
+        self.down04.append(Fire(features[1], features[1]))
+        self.down04.append(Fire(features[1], features[1]))
+        self.down04.append(Fire(features[1], features[1]))
+        # final down sampling (bottle neck)
+        self.down05.append(Fire(features[1], features[2]))
+        self.down05.append(Conv11(features[2], features[2]))
+        self.down05.append(Conv33(features[2], features[2]))
 
         # up sampling part
         for feature in reversed(features):
-            self.ups.append(desqueeze(feature*2, feature))
-            self.ups.append(Conv1(feature*2, feature))
+            self.ups.append(upblock(feature*2, feature))
+        self.ups.append(upblock(features[0], begin_channels))
 
-        # bottle neck
-        self.bottleneck = Conv1(features[-1], features[-1]*2)
+
+        # # bottle neck
+        # self.bottleneck = Conv1(features[-1], features[-1]*2)
 
         # final layer
-        self.finaoconv = finalconv(features[0], in_channels)
+        self.finalconv = finalconv(features[0], in_channels)
     def forward(self, x):
         skip_connections = []
 
-        x = self.begin(x)
+        x = self.down00(x)
         skip_connections.append(x)
         x = self.pools(x)
 
-        for down in self.downs:
-            x = down(x)
-            skip_connections.append(x)
-            x = self.pools(x)
+        x = self.down01(x)
+        skip_connections.append(x)
 
-        x = self.bottleneck(x)
+        x = self.down02(x)
+        skip_connections.append(x)
+        x = self.pools(x)
+
+        x = self.down03(x)
+        x = self.pools(x)
+
+        x = self.down04(x)
+        skip_connections.append(x)
+        x = self.pools(x)
+
+        x = self.down05(x)
 
         skip_connections = skip_connections[::-1]
 
-        for idx in range(0, len(self.ups), 2):
+        for idx in range(0, len(self.ups), 1):
             x = self.ups[idx](x)
-            skip_connections = skip_connections[idx//2]
+            skip_connections = skip_connections[idx]
 
             if x.shape != skip_connections.shape:
                 x = TF.resize(x, size = skip_connections.shape[2:])
 
 
             concat_skip = torch.cat((skip_connections, x), dim = 1)
-            x = self.ips[idx+1](concat_skip)
+            x = self.ups[idx+1](concat_skip)
 
-        return self.final_conv(x)
-
+        return self.finalconv(x)
 
 # test whether net model works fine
 # load the image
 img_path = "datas/wildfireeg001.jpg"
 img = Image.open(img_path)
-# img_tensor = transforms.ToTensor()(img).unsqueeze(0)
-img_tensor = transforms.ToTensor()(img)
+img_tensor = transforms.ToTensor()(img).unsqueeze(0)
+print(img_tensor)
+# img_tensor = transforms.ToTensor()(img)
 plt.imshow(transforms.ToPILImage()(img_tensor))
 model = unetlight()
 
