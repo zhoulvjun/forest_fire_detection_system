de96d4ac563ff53e68df3fb9284c9beae9f19886
lee-shun <lee970802@163.com>
2021-10-14 09:52:14 -0400

    update the local video

--- scripts/develop/pub_local_video.py
+++ scripts/develop/pub_local_video.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*- #
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 #
 #   Copyright (C) 2021 Concordia NAVlab. All rights reserved.
 #
@@ -12,43 +12,37 @@
 #
 #   @Email: 2015097272@qq.com
 #
-#   @Description: 
+#   @Description:
 #
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 
+import os
 import cv2
-import numpy as np
-from std_msgs.msg import Header
-from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 import rospy
+from sensor_msgs.msg import Image
 
-capture = cv2.VideoCapture("../datas/DJI_0261.MP4")
-# capture.set(15, -0.1)
 
 if __name__ == "__main__":
 
-    rospy.init_node('Camera', anonymous=True)  # 定义节点
-    image_pub = rospy.Publisher("dji_osdk_ros/main_camera_images",
-                                Image,
-                                queue_size=10)  # 定义话题
+    video_name = os.path.expanduser("~/catkin_ws/src/forest_fire_detection_system/scripts/develop/datas/DJI_0261.MP4")
+    capture = cv2.VideoCapture(video_name)
+    bridge = CvBridge()
+
+    rospy.init_node('Camera', anonymous=True)
+    image_pub = rospy.Publisher(
+        "dji_osdk_ros/main_camera_images", Image, queue_size=10)
 
     while not rospy.is_shutdown():
-        ret, frame = capture.read()
-        # c_b, c_g, c_r = cv2.split(frame)
-        # frame = cv2.merge([c_r, c_g, c_b])
-        ros_frame = Image()
-        header = Header(stamp=rospy.Time.now())
-        header.frame_id = "Camera"
-        ros_frame.header = header
-        ros_frame.width = 640
-        ros_frame.height = 480
-        ros_frame.encoding = "rgb8"
-        ros_frame.step = 1920
-        ros_frame.data = np.array(frame).tobytes()  # 图片格式转换
-        image_pub.publish(ros_frame)  # 发布消息
 
-        rate = rospy.Rate(10)  # 10hz
+        ret, frame = capture.read()
+        ros_img = bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+        image_pub.publish(ros_img)
+
+        # rospy.loginfo("video from:", video_name)
+
+        rate = rospy.Rate(10)
 
     capture.release()
     cv2.destroyAllWindows()
