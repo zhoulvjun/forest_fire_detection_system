0ca12b82f4ec6c4f1400734d0047c47f1708c765
lee-shun <lee970802@163.com>
2021-10-22 17:08:53 -0400

    update

---
+++ src/single_fire_point_task/single_fire_point_task_manager.cpp
@@ -0,0 +1,21 @@
+/*******************************************************************************
+*
+*   Copyright (C) 2021 Concordia NAVlab. All rights reserved.
+*
+*   @Filename: single_fire_point_task_manager.cpp
+*
+*   @Author: Shun Li
+*
+*   @Email: 2015097272@qq.com
+*
+*   @Date: 2021-10-22
+*
+*   @Description: 
+*
+******************************************************************************/
+
+#include <single_fire_point_task/single_fire_point_task_manager.hpp>
+int main(int argc, char** argv){
+return 0;
+}
+
