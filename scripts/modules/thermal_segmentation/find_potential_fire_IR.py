1fd8e5d97f183e95cd03ad3e461fd10eb6902993
lee-shun <lee970802@163.com>
2021-11-02 11:56:49 -0400

    update the silding window

--- scripts/modules/thermal_segmentation/find_potential_fire_IR.py
+++ scripts/modules/thermal_segmentation/find_potential_fire_IR.py
@@ -4,7 +4,7 @@
 #
 #   Copyright (C) 2021 Concordia NAVlab. All rights reserved.
 #
-#   @Filename: thermal_segmentation.py
+#   @Filename: find_potential_fire_IR.py
 #
 #   @Author: Shun Li
 #
@@ -16,7 +16,6 @@
 #
 # ------------------------------------------------------------------------------
 
-import os
 import numpy as np
 
 import rospy
@@ -25,10 +24,8 @@ from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
 
-import sys
 
-
-class ThermalDetector(object):
+class PotentialFireIrFinder(object):
 
     def __init__(self):
 
@@ -50,66 +47,6 @@ class ThermalDetector(object):
         else:
             self.cv_image = self.convertor.imgmsg_to_cv2(
                 self.ros_image, 'bgr8')
-
-    def gen_mesh(self, width, height, mesh_size):
-
-        int_wid_num = width//mesh_size
-        int_hei_num = height//mesh_size
-        wid_step = [mesh_size for i in range(int_wid_num)]
-        hei_step = [mesh_size for i in range(int_hei_num)]
-
-        wid_step.append(width % mesh_size)
-        hei_step.append(height % mesh_size)
-
-        return wid_step, hei_step
-
-    def image_seg(self, gray, threshold=20):
-
-        ret, binary = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
-
-        # opening
-        kernel = np.ones((2, 2), np.uint8)
-        opening = cv2.morphologyEx(
-            binary, cv2.MORPH_OPEN, kernel, iterations=2)
-
-        wid_step_list, hei_step_list = self.gen_mesh(
-            self.cv_image.shape[1], self.cv_image.shape[0], 20)
-
-        fore_hei = 0
-        back_hei = 0
-        fore_wid = 0
-        back_wid = 0
-
-        coord_list = []
-        judge_list = []
-
-        for wid_step in wid_step_list:
-            fore_wid += wid_step
-            for hei_step in hei_step_list:
-                fore_hei += hei_step
-
-                cur_square = opening[back_hei:fore_hei+1, back_wid:
-                                     fore_wid+1]
-                coord_list.append([fore_hei, back_hei, fore_wid, back_wid])
-                judge_square = cur_square > 0
-                judge_list.append(np.count_nonzero(judge_square))
-
-                back_hei = fore_hei
-
-            fore_hei = 0
-            back_hei = 0
-
-            back_wid = fore_wid
-
-        if (np.count_nonzero(judge_list) != 0):
-            best_index = judge_list.index(max(judge_list))
-            best_pos = coord_list[best_index]
-            print(best_pos)
-            cv2.rectangle(self.cv_image, (best_pos[3], best_pos[1]), (best_pos[2],
-                                                                      best_pos[0]), (255, 255, 255), 1)
-
-        cv2.imshow("img", self.cv_image)
-        cv2.waitKey(3)
 
     def sliding_window(self, image, stepSize=10, windowSize=[20, 20]):
         # slide a window across the image
@@ -141,7 +78,9 @@ class ThermalDetector(object):
 
     def run(self):
         while not rospy.is_shutdown():
+
             if self.cv_image is not None:
+                rospy.loginfo("waiting for the image")
                 _, binary = cv2.threshold(
                     self.cv_image[:, :, 2], 25, 255, cv2.THRESH_BINARY)
 
@@ -150,10 +89,11 @@ class ThermalDetector(object):
                 opening = cv2.morphologyEx(
                     binary, cv2.MORPH_OPEN, kernel, iterations=2)
 
-                self.find(binary)
+                self.find(opening)
 
 
 if __name__ == '__main__':
-    rospy.init_node("thermal_detector_node", anonymous=True)
-    detector = ThermalDetector()
+    rospy.init_node("find_potential_fire_ir_node", anonymous=True)
+    detector = PotentialFireIrFinder()
     detector.run()
+
