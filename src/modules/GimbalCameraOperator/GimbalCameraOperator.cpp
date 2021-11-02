b5fad10c26f1ace4c847049baf8566f06221b6d0
lee-shun <lee970802@163.com>
2021-11-02 16:09:13 -0400

    update the camera contrrol

--- src/modules/GimbalCameraOperator/GimbalCameraOperator.cpp
+++ src/modules/GimbalCameraOperator/GimbalCameraOperator.cpp
@@ -37,30 +37,42 @@ bool GimbalCameraOperator::rotateGimbal(float setPosX, float setPosY,
   while (ros::ok() && (!isTimeOut) && (!isCtrlDone)) {
 
     ros::spinOnce();
+    if (!firePos.is_pot_fire) {
+      PRINT_WARN("not stable potential fire, skip!")
+      pidYaw.reset();
+      pidPitch.reset();
+      continue;
+    } else {
 
-    /* define error */
-    float errX = setPosX - firePos.x;
-    float errY = setPosY - firePos.y;
-    if (errX <= tolErr && errY <= tolErr) {
-      PRINT_INFO(
-          "controling finish after %f seconds with x-error: %f, y-error: %f!",
-          timeinterval, errX, errY);
-      return true;
+      /* define error */
+      float errX = setPosX - firePos.x;
+      float errY = setPosY - firePos.y;
+      if (fabs(errX) <= fabs(tolErr) && fabs(errY) <= fabs(tolErr)) {
+        PRINT_INFO("controling gimbal finish after %f seconds with x-error: "
+                   "%f, y-error: %f!",
+                   timeinterval, errX, errY);
+        return true;
+      }
+
+      pidYaw.ctrl(errX);
+      pidPitch.ctrl(errY);
+
+      dji_osdk_ros::GimbalAction gimbalAction;
+      gimbalAction.request.is_reset = false;
+      gimbalAction.request.payload_index =
+          static_cast<uint8_t>(dji_osdk_ros::PayloadIndex::PAYLOAD_INDEX_0);
+      gimbalAction.request.rotationMode = 0;
+      gimbalAction.request.roll = 0.0f;
+      gimbalAction.request.pitch = pidPitch.fullOutput();
+      gimbalAction.request.yaw = pidYaw.fullOutput();
+      gimbalAction.request.time = 1.0;
+
+      PRINT_DEBUG("pidPitch:%f rad/s", pidPitch.fullOutput());
+      PRINT_DEBUG("pidYaw:%f rad/s", pidYaw.fullOutput());
+
+      gimbal_control_client.call(gimbalAction);
+
     }
-
-    pidYaw.ctrl(errX);
-    pidPitch.ctrl(errY);
-
-    dji_osdk_ros::GimbalAction gimbalAction;
-    gimbalAction.request.is_reset = false;
-    gimbalAction.request.payload_index =
-        static_cast<uint8_t>(dji_osdk_ros::PayloadIndex::PAYLOAD_INDEX_0);
-    gimbalAction.request.rotationMode = 0;
-    gimbalAction.request.pitch = pidPitch.fullOutput();
-    gimbalAction.request.roll = 0.0f;
-    gimbalAction.request.yaw = pidYaw.fullOutput();
-    gimbalAction.request.time = 1.0;
-    gimbal_control_client.call(gimbalAction);
 
     timeinterval = TOOLS::getRosTimeInterval(beginTime);
     if (timeinterval >= timeOut) {
---
+++ src/test/test_GimbalCameraOperator.cpp
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ *
+ *   Copyright (C) 2021 Concordia NAVlab. All rights reserved.
+ *
+ *   @Filename: test_GimbalCameraOperator.cpp
+ *
+ *   @Author: Shun Li
+ *
+ *   @Email: 2015097272@qq.com
+ *
+ *   @Date: 2021-11-02
+ *
+ *   @Description:
+ *
+ ******************************************************************************/
+#include <modules/GimbalCameraOperator/GimbalCameraOperator.hpp>
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "test_GimbalCameraOperator_node");
+  FFDS::MODULES::GimbalCameraOperator gimbalCameraOperator;
+  gimbalCameraOperator.rotateGimbal(300, 200, 10.0, 10.0);
+
+  return 0;
+}
